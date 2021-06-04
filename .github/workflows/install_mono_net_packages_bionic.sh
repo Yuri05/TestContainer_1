60bb6049cd36baa3ad2b2bded58f7d2743ca31a5
Yuri05 <Yuri05@github.com>
2021-06-04 16:58:00 +0200

    GHCR Test 1

--- .github/workflows/install_mono_net_packages_bionic.sh
+++ .github/workflows/install_mono_net_packages_bionic.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apt-get -y update
-apt-get -y install openmpi-bin openmpi-common libopenmpi-dev wget
+apt-get -y install git nuget libcurl4-openssl-dev libssl-dev libxml2-dev openmpi-bin openmpi-common libopenmpi-dev wget
 locale-gen en_US.UTF-8
